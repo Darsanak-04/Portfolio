efe7d711faa1b1fcc214f9726fe983d3e2687f7c
Darsanak-04 <darsanashanthi@gmail.com>
2025-06-26 12:59:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Update the current year in the footer
+    const currentYearSpan = document.getElementById('current-year');
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+});
